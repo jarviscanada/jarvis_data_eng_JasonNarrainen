81529aede95dc025e79ea01bea85af9eb257bc71
Jason Narrainen <jason.narrainen@protonmail.com>
2023-08-09 16:58:01 -0400

    Added initial barebones code

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -0,0 +1,91 @@
+package ca.jrvs.apps.grep;
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class JavaGrepImp implements JavaGrep{
+
+    final Logger logger = LoggerFactory.getLogger(JavaGrep.class);
+    private String regex;
+    private String rootPath;
+    private String outFile;
+
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("USAGE: JavaGrep regex rootPath outFile");
+        }
+
+        BasicConfigurator.configure();
+
+        JavaGrepImp javaGrepImp = new JavaGrepImp();
+        javaGrepImp.setRegex(args[0]);
+        javaGrepImp.setRootPath(args[1]);
+        javaGrepImp.setOutFile(args[2]);
+
+        try {
+            javaGrepImp.process();
+        } catch (Exception e) {
+            javaGrepImp.logger.error("Error: Unable to process", e);
+        }
+    }
+
+    @Override
+    public void process() throws IOException {
+
+    }
+
+    @Override
+    public List<File> listFiles(String rootDir) {
+        return null;
+    }
+
+    @Override
+    public List<String> readLines(File inputFile) {
+        return null;
+    }
+
+    @Override
+    public boolean containsPattern(String line) {
+        return false;
+    }
+
+    @Override
+    public void writeToFile(List<String> lines) throws IOException {
+
+    }
+
+    @Override
+    public String getRootPath() {
+        return this.rootPath;
+    }
+
+    @Override
+    public void setRootPath(String rootPath) {
+        this.rootPath = rootPath;
+    }
+
+    @Override
+    public String getRegex() {
+        return this.regex;
+    }
+
+    @Override
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+
+    @Override
+    public String getOutFile() {
+        return this.outFile;
+    }
+
+    @Override
+    public void setOutFile(String outFile) {
+        this.outFile = outFile;
+    }
+}
